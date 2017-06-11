a6d072583d0fc9c7dc1a526d289a99c563ab0d99
Anisa <anisa.ali7@gmail.com>
2017-06-11 18:22:28 -0500

    url with name

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# @app.route('/hello')
+# def hello_world():
+#     return 'Hello World!'
+
+# If URL localhost:5000/hello/May will render Hello May
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' % name
 
 if __name__ == '__main__':
     app.run()
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=5000, debug=False)
